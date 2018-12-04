944bc3fb76bbe79cff583a09f188c109f6a54b18
matteotoccafondi <toccafondi2000@gmail.com>
2018-12-04 09:18:35 +0100

    first commit

---
+++ compito.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     for(int i=0;i<10;i++){
+             cout<<"Variabile contatore: "<<i<<endl;        
+     } 
+     cout<<"Fine"<<endl;
+     system("pause");
+     return 0;
+} 
